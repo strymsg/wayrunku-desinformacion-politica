c8ddb7b19e5bbbd6fd305efd9cce4dc42340aced
Rodrigo Garcia <rgarcia@laotra.red>
2025-07-30 11:11:38 -0400

    Actualizando hasta el 30 de Julio

--- reports.py
+++ reports.py
@@ -29,8 +29,8 @@ engine = db.engine
 
 DESTINY_FOLDER='./data/reports/'
 
-SNAPSHOT_DATE_FROM = '2025-07-22'
-SNAPSHOT_DATE_TO = '2025-07-23'
+SNAPSHOT_DATE_FROM = '2025-07-29'
+SNAPSHOT_DATE_TO = '2025-07-30'
 
 @click.command()
 @click.option(
